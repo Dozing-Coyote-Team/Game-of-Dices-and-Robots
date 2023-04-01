a987f980cb45dc20599ab22f0b489754a8b5c462
Cristian <cri.ghiorzi@gmail.com>
2023-04-01 09:11:07 +0200

    Equipment slots Player class

--- Assets/Scripts/Actor/Player/Player.cs
+++ Assets/Scripts/Actor/Player/Player.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Player : Actor
 {
-   
+    [Header("Settings")]
+    [SerializeField]
+    private List<Equipment> _equipmentSlots;
+
+    private void ActiveEquipmentsAbility(List<int> dicesResults)
+    {
+        foreach(Equipment equipment in _equipmentSlots)
+        {
+            if(equipment == null)
+                continue;
+
+            equipment.ActivateEffect(dicesResults);
+        }
+    }
 }
