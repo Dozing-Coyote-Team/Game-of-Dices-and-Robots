2108401dd368d66f721f2bbd88eaf3cd0753bee6
nico-bertoli <nicobertoli.1999@gmail.com>
2023-04-02 00:02:47 +0200

    360 animation working

--- Assets/Scripts/Dice/UIDieModel.cs
+++ Assets/Scripts/Dice/UIDieModel.cs
@@ -8,6 +8,9 @@ using UnityEngine.UI;
 public class UIDieModel : MonoBehaviour
 {
     //--------------------------- private vars
+    [SerializeField] private float _animDuration = 3f;
+    [SerializeField] private AnimationCurve _animationCurve; 
+    
     private Animator _animator;
 
     //--------------------------- public methods
@@ -23,5 +26,39 @@ public class UIDieModel : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
-    private void AnimatorCalled_RollAnimEnd() { OnRollAnimEnd(); }
+    private void Start()
+    {
+        StartCoroutine(Rotate(1080));
+    }
+    
+    private IEnumerator Rotate(float totalRot)
+    {
+        float t = 0;
+        float currentRot = 0;
+        float previousRot = 0;
+
+        float frameRot;
+
+        float timeElapsed = 0;
+
+        while (timeElapsed < _animDuration)
+        {
+            t = timeElapsed / _animDuration;
+            t = _animationCurve.Evaluate(t);
+            
+            currentRot = Mathf.Lerp(0, totalRot, t);
+
+            frameRot = currentRot - previousRot;
+            
+            Debug.Log("frame rot: "+frameRot);
+            
+            transform.RotateAround(transform.position,Vector3.up + Vector3.right,frameRot);
+
+            previousRot = currentRot;
+            timeElapsed += Time.deltaTime;
+            
+            yield return null;
+        }
+    }
+
 }
