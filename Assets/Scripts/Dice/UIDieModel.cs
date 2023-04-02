aa09107353decd7ada35b46c92d0aeaf3785f785
nico-bertoli <nicobertoli.1999@gmail.com>
2023-04-02 23:45:09 +0200

    rotations working

--- Assets/Scripts/Dice/UIDieModel.cs
+++ Assets/Scripts/Dice/UIDieModel.cs
@@ -25,65 +25,38 @@ public class UIDieModel : MonoBehaviour
     [SerializeField] private UIDieModelRotation _upRotation;
     [SerializeField] private UIDieModelRotation _bottomRotation;
 
-    private int _currentResult = 1;
+    // private int _currentResult = 1;
+
+    private float _diceRadius;
     //--------------------------- public methods
     public void RollTo(int newResult)
     {
         UIDieModelRotation rotation = EvaluateRotationTo(newResult);
-        
-        Debug.Log("Evaluated: "+rotation.name);
-        
+
         StartCoroutine(Rotate(360 * _numRolls + rotation.RotationOffset,rotation.Axis));
     }
 
     private UIDieModelRotation EvaluateRotationTo(int newResult)
     {
-        if (newResult == _currentResult)
-            return _frontRotations[Random.Range(0, _frontRotations.Count)];
-        
+
         Vector3 newResultPos = _faceIndicators[newResult -1].position;
-        Vector3 currentResutPos = _faceIndicators[_currentResult-1].position;
-        
-        if (newResultPos.y > currentResutPos.y)
+
+        if (newResultPos.y > transform.position.y+_diceRadius/2)
             return _upRotation;
-        else if (newResultPos.y < currentResutPos.y)
+        else if (newResultPos.y < transform.position.y-_diceRadius/2)
             return _bottomRotation;
-        else if (newResultPos.x > currentResutPos.x)
+        else if (newResultPos.x > transform.position.x+_diceRadius/2)
             return _rightRotation;
-        else if (newResultPos.x < currentResutPos.x)
+        else if (newResultPos.x < transform.position.x-_diceRadius/2)
             return _leftRotaion;
-        else
+        else if(newResultPos.z > transform.position.z + _diceRadius/2)
             return _backRotation;
+        else
+            return _frontRotations[Random.Range(0, _frontRotations.Count)];
     }
 
     public Action OnRollAnimEnd;
     //--------------------------- private methods
-    
-    // USATE
-    // ================================================== left e bottom 
-    // bottom => StartCoroutine(Rotate(360 * _numRolls + 120,Vector3.up + Vector3.right + Vector3.forward));
-    // bottom => StartCoroutine(Rotate(360 * _numRolls - 120,Vector3.up + Vector3.right + Vector3.forward));
-    
-    // ================================================== right e top
-    //right => StartCoroutine(Rotate(360 * _numRolls + 120,Vector3.up + Vector3.right + Vector3.back));
-    //top => StartCoroutine(Rotate(360 * _numRolls - 120,Vector3.up + Vector3.right + Vector3.back));
-    
-    // ================================================== front e back (bella)
-    // front => StartCoroutine(Rotate(360 * _numRolls, Vector3.up + Vector3.right));
-    // back => StartCoroutine(Rotate(360 * _numRolls + 180, Vector3.up + Vector3.right));
-    
-    //NON USATE
-    
-    // ================================================== right e bottom
-    // right => StartCoroutine(Rotate(360 * _numRolls + 120,Vector3.up + Vector3.left + Vector3.forward));
-    // bottom => StartCoroutine(Rotate(360 * _numRolls - 120,Vector3.up + Vector3.left + Vector3.forward));
-    // front => StartCoroutine(Rotate(360 * _numRolls,Vector3.up + Vector3.left + Vector3.forward));
-
-    //==================================================  left e top
-    // top => StartCoroutine(Rotate(360 * _numRolls + 120,Vector3.up + Vector3.left + Vector3.back));
-    // left => StartCoroutine(Rotate(360 * _numRolls - 120,Vector3.up + Vector3.left + Vector3.back));
-    
-    
     
     private IEnumerator Rotate(float totalRot,Vector3 aroundAxis)
     {
@@ -113,4 +86,34 @@ public class UIDieModel : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        _diceRadius = Vector3.Distance(transform.position, _faceIndicators[0].position);
+    }
 }
+
+
+
+// USATE
+// ================================================== left e bottom 
+// bottom => StartCoroutine(Rotate(360 * _numRolls + 120,Vector3.up + Vector3.right + Vector3.forward));
+// left => StartCoroutine(Rotate(360 * _numRolls - 120,Vector3.up + Vector3.right + Vector3.forward));
+    
+// ================================================== right e top
+//right => StartCoroutine(Rotate(360 * _numRolls + 120,Vector3.up + Vector3.right + Vector3.back));
+// top => StartCoroutine(Rotate(360 * _numRolls - 120,Vector3.up + Vector3.right + Vector3.back));
+    
+// ================================================== front e back (bella)
+// front => StartCoroutine(Rotate(360 * _numRolls, Vector3.up + Vector3.right));
+// back => StartCoroutine(Rotate(360 * _numRolls + 180, Vector3.up + Vector3.right));
+    
+//NON USATE
+    
+// ================================================== right e bottom
+// right => StartCoroutine(Rotate(360 * _numRolls + 120,Vector3.up + Vector3.left + Vector3.forward));
+// bottom => StartCoroutine(Rotate(360 * _numRolls - 120,Vector3.up + Vector3.left + Vector3.forward));
+// front => StartCoroutine(Rotate(360 * _numRolls,Vector3.up + Vector3.left + Vector3.forward));
+
+//==================================================  left e top
+// top => StartCoroutine(Rotate(360 * _numRolls + 120,Vector3.up + Vector3.left + Vector3.back));
+// left => StartCoroutine(Rotate(360 * _numRolls - 120,Vector3.up + Vector3.left + Vector3.back));
