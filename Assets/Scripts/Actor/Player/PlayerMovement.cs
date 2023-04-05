5ec36e7dbf09c38350a54d2055e74ed5461457fa
Luca Biagioli <79144594+lucabigio@users.noreply.github.com>
2023-04-06 00:29:36 +0200

    Update PlayerMovement.cs

--- Assets/Scripts/Actor/Player/PlayerMovement.cs
+++ Assets/Scripts/Actor/Player/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : ActorMovement
 
     [Header("Player References")]
     [SerializeField]
-    private PlayerCombat playerCombat;
+    private PlayerCombat _playerCombat;
 
     protected override void OnEnable()
     {
